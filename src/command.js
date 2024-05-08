49bd266fa2c5d3d733232f419ea94072b4c2c02c
DanieKombou <danielkombou7@gmail.com>
2024-05-08 11:35:54 +0100

    crud method

--- src/command.js
+++ src/command.js
@@ -1,6 +1,16 @@
 import yargs from 'yargs'
 import {hideBin} from 'yargs/helpers'
-import { newNote } from './note.js'
+import { findNotes, getAllNotes, newNote, removeAllNotes, removeNote } from './note.js'
+
+
+const listNotes = notes =>{
+  notes.forEach(({id, content, tags}) => {
+    console.log('id: ', id);
+    console.log('tags: ', tags);
+    console.log('content: ', content);
+    console.log('\n');
+  });
+}
 
 
 yargs(hideBin(process.argv))
@@ -10,16 +20,21 @@ yargs(hideBin(process.argv))
         description: 'The content of the note to create',
     })
 }, async (argv) =>{
-    const tags = argv.tags ? argv.tags.split(',') : []
+    const tags = argv._;
+    console.log(argv._);
     const note = await newNote(argv.note, tags)
     console.log('New note!', note);
+    // console.log(tags);
 })
 .option('tags', {
     alias: 't',
     type: 'string',
     description: 'tags to add to the note'
 })
-.command('all', 'get all notes', () =>{}, async (argv) =>{})
+.command('all', 'get all notes', () =>{}, async (argv) =>{
+  const notes = await getAllNotes()
+  listNotes(notes)
+})
 
 .command('find <filter>', 'get matching notes', yargs => {
     return yargs.positional('filter', {
@@ -27,7 +42,8 @@ yargs(hideBin(process.argv))
       type: 'string'
     })
   }, async (argv) => {
-    
+    const matches = await findNotes(argv.filter)
+    listNotes(matches)
   })
   .command('remove <id>', 'remove a note by id', yargs => {
     return yargs.positional('id', {
@@ -35,7 +51,8 @@ yargs(hideBin(process.argv))
       description: 'The id of the note you want to remove'
     })
   }, async (argv) => {
-    
+    const id = await removeNote(argv.id)
+    console.log(id);
   })
   .command('web [port]', 'launch website to see notes', yargs => {
     return yargs
@@ -48,7 +65,8 @@ yargs(hideBin(process.argv))
     
   })
   .command('clean', 'remove all notes', () => {}, async (argv) => {
-    
+    await removeAllNotes()
+    console.log('db reseted');
   })
 
 .demandCommand(1)
--- test.js
+++ test.js
@@ -3,12 +3,12 @@ import fs from 'node:fs/promises'
 
 const readPjson = async () => {
     const pjsonPath = new URL('./package.json', import.meta.url).pathname
-    console.log(JSON.parse(await fs.readFile(pjsonPath, 'utf-8')));
+    console.log(JSON.parse(await fs.readFile('./package.json', 'utf-8')));
 }
 
 const writeFile = async () =>{
     const newFile = new URL('./demo.js', import.meta.url).pathname
-    await fs.writeFile(newFile, `console.log('yoooo!')`)
+    await fs.writeFile('./demo.js', `console.log('yoooo!')`)
 }
 
 // readPjson()
